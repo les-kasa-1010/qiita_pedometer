ec923ab7b6ab28c29e5aeb7d3bf53e846a5cb133
les-kasa <3480914+le-kamba@users.noreply.github.com>
2021-03-22 01:29:45 +0900

    Koin-Scopeに対応したBaseActivityを追加

--- app/src/main/java/jp/les/kasa/sample/mykotlinapp/base/BaseActivity.kt
+++ app/src/main/java/jp/les/kasa/sample/mykotlinapp/base/BaseActivity.kt
@@ -3,9 +3,23 @@ package jp.les.kasa.sample.mykotlinapp.base
 import androidx.appcompat.app.AppCompatActivity
 import jp.les.kasa.sample.mykotlinapp.utils.AnalyticsUtilI
 import org.koin.android.ext.android.inject
+import org.koin.androidx.scope.ScopeActivity
 
 // Analytics送信を基底クラスに持たせる場合のサンプル
 abstract class BaseActivity : AppCompatActivity() {
+
+    abstract val screenName: String
+
+    // AnalyticsTool inject by Koin
+    val analyticsUtil: AnalyticsUtilI by inject()
+
+    override fun onResume() {
+        super.onResume()
+        analyticsUtil.sendScreenName(screenName)
+    }
+}
+
+abstract class ScopeBaseActivity : ScopeActivity() {
 
     abstract val screenName: String
 
